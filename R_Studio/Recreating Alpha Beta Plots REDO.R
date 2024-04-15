9e99c5c0b4f098eb79123728227c818d8929ce0d
loujainbilal <159094203+loujainbilal@users.noreply.github.com>
2024-04-15 15:02:02 -0700

    Update Recreating Alpha Beta Plots REDO.R

--- R_Studio/Recreating Alpha Beta Plots REDO.R
+++ R_Studio/Recreating Alpha Beta Plots REDO.R
@@ -1,4 +1,3 @@
-####RECREATING ALPHA AND BETA PLOTS IN R STUDIO ####
 ### Load packages ###
 library(phyloseq)
 library(ape) 
@@ -71,38 +70,71 @@ save(team4_final, file="team4_final.RData")
 save(team4_rare, file="team4_rare.RData")
 
 
-#### Alpha diversity ######
+### ALPHA DIVERSITY ###
 
-## richness ##
+## RICHNESS ##
 
 plot_richness(team4_rare) 
 
-plot_richness(team4_rare, measures = c("Shannon")) 
+plot_richness(team4_rare, measures = c("Observed", "Shannon","Chao1")) 
 
 #hba1c_prediabetic#
-gg_richness <- plot_richness(team4_rare, x = "hba1c_prediabetic", measures = c("Shannon")) +
+gg_richness_hba1c <- plot_richness(team4_rare, x = "hba1c_prediabetic", measures = c("Observed", "Shannon","Chao1")) +
   xlab("HbA1c Prediabetic Status") + 
   geom_boxplot(aes())
-gg_richness
+gg_richness_hba1c
 
-ggsave(filename = "plot_shannon_hba1c.png"
-       , gg_richness
+
+ggsave(filename = "plot_three_hba1c.png"
+       , gg_richness_hba1c
+       , height=4, width=6)
+
+#kruskal-wallis test for hba1c#
+
+kruskal_sh_hba1c <- kruskal.test(Shannon ~ `hba1c_prediabetic`, data = samp_dat_wdiv)
+# p-value = 0.08343, ns 
+
+kruskal_chao1_hba1c <- kruskal.test(Chao1 ~ `hba1c_prediabetic`, data = samp_dat_wdiv)
+# p-value = 0.1942, ns 
+
+kruskal_observed_hba1c <- kruskal.test(Observed ~ `hba1c_prediabetic`, data = samp_dat_wdiv)
+# p-value = 0.1659, ns 
+
+
+#fast_glucose_prediabetic#
+gg_richness_fpg <- plot_richness(team4_rare, x = "fast_glucose_prediabetic", measures = c("Observed", "Shannon","Chao1")) +
+  xlab("Fasting Plasma Glucose (FPG) Prediabetic Status") +
+  geom_boxplot()
+gg_richness_fpg
+
+ggsave(filename = "plot_three_fast_glucose.png"
+       , gg_richness_fpg
        , height=4, width=6)
 
 
 #fast_glucose_prediabetic#
-gg_richness <- plot_richness(team4_rare, x = "fast_glucose_prediabetic", measures = c("Shannon")) +
-  xlab("Fasting Glucose Prediabetic Status") +
+gg_richness_fpg <- plot_richness(team4_rare, x = "fast_glucose_prediabetic", measures = c( "Shannon")) +
+  xlab("Fasting Plasma Glucose (FPG) Prediabetic Status") +
   geom_boxplot()
-gg_richness
+gg_richness_fpg
 
 ggsave(filename = "plot_shannon_fast_glucose.png"
-       , gg_richness
-       , height=4, width=6)
+       , gg_richness_fpg
+       , height=4, width=3)
+
+#kruskal-wallis test for fasting glucose#
+kruskal_observed_fast <- kruskal.test(Observed ~ `fast_glucose_prediabetic`, data = samp_dat_wdiv)
+# p-value = 0.5528, ns 
+
+kruskal_chao1_fast <- kruskal.test(Chao1 ~ `fast_glucose_prediabetic`, data = samp_dat_wdiv)
+# p-value = 0.6126, ns 
+
+kruskal_sh_fast <- kruskal.test(Shannon ~ `fast_glucose_prediabetic`, data = samp_dat_wdiv)
+# p-value = 0.35, ns 
 
 
 
-## phylogenetic diversity ##
+## FAITH'S PD ##
 
 #hba1c_prediabetic#
 phylo_dist <- pd(t(otu_table(team4_rare)), phy_tree(team4_rare),
@@ -119,7 +151,7 @@ plot.pd
 
 ggsave(filename = "pd_hba1c_prediabetic.png"
        , plot.pd
-       , height=4, width=6)
+       , height=5, width=3)
 
 
 #fast_glucose_prediabetic#
@@ -137,12 +169,22 @@ plot.pd
 
 ggsave(filename = "pd_glucose_prediabetic.png"
        , plot.pd
-       , height=4, width=6)
+       , height=4, width=3)
 
 
-#### Beta diversity #####
+#### BETA DIVERSITY - PERMANOVA ######
 
-## bray curtis ##
+## JACCARD ##
+
+dm_jaccard <- vegdist(t(otu_table(team4_rare)), method="jaccard")
+adonis2(dm_jaccard ~ `hba1c_prediabetic`*`fast_glucose_prediabetic`, data=samp_dat_wdiv)
+
+#                                             Df SumOfSqs      R2      F Pr(>F)
+#hba1c_prediabetic                            1    0.422 0.00335 1.0352  0.338
+#fast_glucose_prediabetic                     1    0.388 0.00308 0.9506  0.584
+#hba1c_prediabetic:fast_glucose_prediabetic   1    0.369 0.00293 0.9051  0.835
+
+## BRAY CURTIS ##
 
 #hba1c_prediabetic#
 bc_dm <- distance(team4_rare, method="bray")
@@ -235,6 +277,43 @@ ggsave("plot_pcoa_wf_hba1c.png"
        , gg_pcoa_wf_hba1c
        , height=4, width=5)
 
+##WEIGHTED UNIFRAC - HbA1c##
+
+dm_unifrac_hba1c <- UniFrac(team4_rare, weighted=TRUE)
+
+ord.unifrac <- ordinate(team4_rare, method="PCoA", distance="unifrac")
+plot_ordination(team4_rare, ord.unifrac, color="hba1c_prediabetic") +
+  stat_ellipse(type = "norm")
+
+adonis2(dm_unifrac_hba1c ~ `hba1c_prediabetic`*`fast_glucose_prediabetic`, data=samp_dat_wdiv)
+#                                           Df SumOfSqs      R2      F Pr(>F)
+#hba1c_prediabetic                            1 0.000795 0.00412 1.2695  0.256
+#fast_glucose_prediabetic                     1 0.000267 0.00138 0.4267  0.820
+#hba1c_prediabetic:fast_glucose_prediabetic   1 0.000358 0.00186 0.5719  0.495
+
+## Saving plot ##
+ggsave("plot_pcoa_wf_hba1c_2.png"
+       , dm_unifrac_hba1c
+       , height=4, width=5)
+
+##WEIGHTED UNIFRAC - FPG##
+
+dm_unifrac_fpg <- UniFrac(team4_rare, weighted=TRUE)
+
+ord.unifrac <- ordinate(team4_rare, method="PCoA", distance="unifrac")
+plot_ordination(team4_rare, ord.unifrac, color="fast_glucose_prediabetic") +
+  stat_ellipse(type = "norm")
+
+adonis2(dm_unifrac_fpg ~ `hba1c_prediabetic`*`fast_glucose_prediabetic`, data=samp_dat_wdiv)
+
+#                                            Df SumOfSqs      R2      F Pr(>F)
+#hba1c_prediabetic                            1 0.000795 0.00412 1.2695  0.288
+#fast_glucose_prediabetic                     1 0.000267 0.00138 0.4267  0.778
+#hba1c_prediabetic:fast_glucose_prediabetic   1 0.000358 0.00186 0.5719  0.526
+
+
+
+
 
 #fast_glucose_prediabetic#
 wunifrac_dis <- distance(team4_rare, method="wunifrac")
@@ -252,5 +331,3 @@ gg_pcoa_wf_fast
 ggsave("plot_pcoa_wf_fast.png"
        , gg_pcoa_wf_fast
        , height=4, width=5)
-
-#By Loujain
